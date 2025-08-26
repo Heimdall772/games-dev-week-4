16b4e2e9a89627d010088ed737a6eac1239a65cb
Heimdall772 <thakursyuvraj@gmail.com>
2025-08-26 21:45:41 +1000

    second commit

---
+++ Assets/Scripts/LoadAssets.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadAssets : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+
+        Instantiate(redObj, new Vector3(2.2f, 0f, 0f), Quaternion.identity);
+
+        Instantiate(blueObj, new Vector3(-2.2f, 0f, 0f), Quaternion.identity);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject redObj;
+
+    [SerializeField]
+    private GameObject blueObj;
+}
