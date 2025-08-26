fc641fc0722186e36a02c31e07add39bbc5b4eeb
Heimdall772 <thakursyuvraj@gmail.com>
2025-08-26 22:09:53 +1000

    commit

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsolePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -18,5 +18,7 @@ public class ConsolePrint : MonoBehaviour
 
     }
     private int i = 3;
+
+    public Renderer rend;
 
 }
